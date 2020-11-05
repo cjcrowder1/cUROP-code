dbe1f43aac448955eb7e6e4cf1d7d2dc01a54e86
Caroline Crowder <carolinecrowder@Carolines-MacBook-Air.local>
2020-11-05 09:55:41 -0500

    Added for UROP assignment

---
+++ caroline_code.py
@@ -0,0 +1,19 @@
+import math
+
+def my_square_root(val):
+    if val < 0:
+        return -1
+    else:
+        return math.sqrt(val)
+
+if __name__ == '__main__':
+    print("This is Caroline's first piece of code on her own repository!")
+
+def main():
+    x=Carolines_repository()
+    print(x)
+
+def Carolines_repository():
+    return 'The exercise for this week.'
+
+if __name__ == '__main__': main()
